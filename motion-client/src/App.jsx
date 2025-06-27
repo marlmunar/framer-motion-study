c135324f64df4ab5040821073176773f411d06fc
marlmunar <marlemmmunar@gmail.com>
2025-06-27 14:39:39 +0800

    Added whileHover, whileTap, and drag animations

--- motion-client/src/App.jsx
+++ motion-client/src/App.jsx
@@ -1,44 +1,79 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Div from "./components/Div";
 import "./index.css";
-import { AnimatePresence, motion } from "framer-motion";
+import {
+  AnimatePresence,
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+} from "framer-motion";
 
 const App = () => {
   const [isVisible, setIsVisible] = useState(true);
+  const constraintsRef = useRef(null);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const limits = {
+    position: "relative",
+    width: 300,
+    height: 300,
+    backgroundColor: "cornsilk",
+    borderRadius: 10,
+  };
+
+  const box = {
+    width: "10rem",
+    height: "10rem",
+  };
 
   return (
     <main>
-      <AnimatePresence mode="wait">
-        {isVisible ? (
+      <motion.div ref={constraintsRef} style={limits}>
+        <AnimatePresence mode="wait">
           <motion.div
+            style={{ ...box, x, y }}
+            drag
+            dragConstraints={constraintsRef}
+            onDragEnd={() => {
+              console.log("Final position:", x.get(), y.get());
+            }}
             key={isVisible ? "visible" : "hidden"}
-            initial={{ opacity: 0, x: -50 }}
-            animate={{
-              x: [-50, 10, 0],
-              opacity: [0, 0.5, 1],
-              transition: {
-                duration: 1.25,
-                times: [0, 0.75, 1],
-              },
-            }}
-            exit={{
-              y: [10, 0, -100],
-              opacity: [1, 0.85, 0],
-              transition: { duration: 1, times: [0, 0.85, 1] },
-            }}
+            initial={isVisible ? { opacity: 0 } : { y: 0 }}
+            animate={
+              isVisible
+                ? {
+                    x: [-50, 10, 0],
+                    opacity: [0, 0.5, 1],
+                    transition: {
+                      duration: 1.25,
+                      times: [0, 0.75, 1],
+                    },
+                  }
+                : {
+                    y: [10, 0, -100],
+                    opacity: [1, 0.85, 0],
+                    scale: [0.9, 1, 0.65],
+                    transition: { duration: 1, times: [0, 0.85, 1] },
+                  }
+            }
           >
             <Div />
           </motion.div>
-        ) : (
-          <div style={{ visibility: "hidden" }}>
-            <Div />
-          </div>
-        )}
-      </AnimatePresence>
+        </AnimatePresence>
+      </motion.div>
 
-      <button className="btn" onClick={() => setIsVisible((prev) => !prev)}>
+      <motion.button
+        className="btn"
+        whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+        whileTap={{ scale: 0.95, rotate: 0.5 }}
+        onClick={() => {
+          setIsVisible((prev) => !prev);
+          console.log(x.get(), y.get());
+        }}
+      >
         Show/Hide
-      </button>
+      </motion.button>
     </main>
   );
 };
